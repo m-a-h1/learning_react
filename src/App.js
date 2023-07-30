21c64f4dd22de6caae5dd5e5a0841d2226f2b2bd
amir <amirtnt80@gmail.com>
2023-07-30 15:12:23 +0330

    save changes

--- src/App.js
+++ src/App.js
@@ -1,22 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import LearningReact from "./LearnReact";
+import { useEffect, useState } from "react";
 
 function App() {
+  const [customerList, setCustomerList] = useState([]);
+
+  const callApi = () => {
+    return [1, 2, 3, 4, 5];
+  };
+
+  const getCustomer = () => {
+    const data = callApi();
+
+    setCustomerList(data);
+  };
+
+  console.log(">>>>> here in component ");
+
+  useEffect(() => {
+    setTimeout(() => {
+      getCustomer();
+    }, 2000);
+  }, []);
+
   return (
     <div className="App">
       <header className="App-header">
+        {}
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+
+        {customerList.map((item) => (
+          <span>{item}</span>
+        ))}
+
+        <LearningReact />
       </header>
     </div>
   );
---
+++ src/LearnReact.jsx
@@ -0,0 +1,9 @@
+const LearningReact = () => {
+  return (
+    <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
+      Learn React
+    </a>
+  );
+};
+
+export default LearningReact;
